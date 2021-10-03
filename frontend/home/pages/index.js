9abb18f4ed2a83c8d800e10d7e24d9c489577d83
cntran <cntran@gmail.com>
2021-10-03 13:04:22 -0700

    update home page

--- frontend/home/pages/index.js
+++ frontend/home/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Home MicroApp
+          Home MicroApp - Update
         </h1>
 
         <p className={styles.description}>
